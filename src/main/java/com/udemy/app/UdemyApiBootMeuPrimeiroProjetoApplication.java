788c02e1dc424a4ca875aa8933832da06a61de58
Silvio Angelo <silvioangels@gmail.com>
2018-06-29 13:48:30 -0300

    Versao Inicial

---
+++ src/main/java/com/udemy/app/UdemyApiBootMeuPrimeiroProjetoApplication.java
@@ -0,0 +1,12 @@
+package com.udemy.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyApiBootMeuPrimeiroProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyApiBootMeuPrimeiroProjetoApplication.class, args);
+	}
+}
